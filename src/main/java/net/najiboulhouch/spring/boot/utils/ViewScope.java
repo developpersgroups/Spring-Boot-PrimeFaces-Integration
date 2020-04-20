17572a2f9050f8a756a23ac02bf45b3291e35c00
developpersgroups <developpersgroups@gmail.com>
2020-04-20 05:40:48 +0000

    Add utils package

---
+++ src/main/java/net/najiboulhouch/spring/boot/utils/ViewScope.java
@@ -0,0 +1,45 @@
+package net.najiboulhouch.spring.boot.utils;
+
+import java.util.Map;
+
+import javax.faces.context.FacesContext;
+
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.config.Scope;
+
+
+/**
+ * @author NAJIB
+ * @version 1.0 
+ */
+public class ViewScope implements Scope {
+
+	public Object get(String name, ObjectFactory<?> objectFactory) {
+		Map<String, Object> viewMap = FacesContext.getCurrentInstance().getViewRoot().getViewMap();
+		
+		if(viewMap.containsKey(name)) {
+			return viewMap.get(name);
+		} else {
+			Object object = objectFactory.getObject();
+			viewMap.put(name, object);
+			return object;
+		}
+	}
+
+	public String getConversationId() {
+		return null;
+	}
+
+	public void registerDestructionCallback(String arg0, Runnable arg1) {
+		
+	}
+
+	public Object remove(String name) {
+		return FacesContext.getCurrentInstance().getViewRoot().getViewMap().remove(name);
+	}
+
+	public Object resolveContextualObject(String arg0) {
+		return null;
+	}
+
+}
