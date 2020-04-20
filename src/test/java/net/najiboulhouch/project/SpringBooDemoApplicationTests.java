b6900860f80f0b8255f5b73bd07689b3d769cbeb
developpersgroups <developpersgroups@gmail.com>
2020-04-20 05:45:40 +0000

    Add spring boot test class

---
+++ src/test/java/net/najiboulhouch/project/SpringBooDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package net.najiboulhouch.project;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBooDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
